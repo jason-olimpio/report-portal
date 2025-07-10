cc9fca82bfaa050d3c3f58c5a5309de4452ec82d
Jason Olimpio <j.olimpio@fm-technology.it>
2025-07-10 11:50:13 +0200

    fix: update background color for report not found view for improved visibility

--- screens/report/ReportDetailsScreen.tsx
+++ screens/report/ReportDetailsScreen.tsx
@@ -54,7 +54,7 @@ const ReportDetailsScreen = () => {
 
   if (!report) {
     return (
-      <View className="flex-1 bg-white">
+      <View className="flex-1 bg-background-light dark:bg-background-dark">
         <Text className="text-center mt-6 text-red-500">
           {t('reportNotFound')}
         </Text>
