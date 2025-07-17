b715a28eefb516ae9675b8104ad6f74776c3e6d5
Jason Olimpio <j.olimpio@fm-technology.it>
2025-07-17 16:45:20 +0200

    refactor: remove unnecessary View wrapper in LoginRegisterTabs for cleaner structure

--- components/navigation/tabs/LoginRegisterTabs.tsx
+++ components/navigation/tabs/LoginRegisterTabs.tsx
@@ -1,6 +1,6 @@
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {useTranslation} from 'react-i18next';
-import {View, SafeAreaView} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
 import {LoginScreen, RegisterScreen} from '@screens';
 import {useTheme} from '@hooks';
@@ -15,56 +15,54 @@ const LoginRegisterTabs = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-background-light dark:bg-background-dark">
-      <View className="flex-1">
-        <Tab.Navigator
-          screenOptions={{
-            tabBarStyle: {
-              backgroundColor: isDark
-                ? appColors.background.dark
-                : appColors.background.light,
-              elevation: 0,
-              shadowOpacity: 0,
-              borderBottomWidth: 1,
-              borderBottomColor: isDark
-                ? appColors.neutral.gray[700]
-                : appColors.neutral.gray[200],
-            },
-            tabBarLabelStyle: {
-              fontSize: 16,
-              fontWeight: '600',
-              textTransform: 'none',
-            },
-            tabBarActiveTintColor: isDark
+      <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: isDark
+              ? appColors.background.dark
+              : appColors.background.light,
+            elevation: 0,
+            shadowOpacity: 0,
+            borderBottomWidth: 1,
+            borderBottomColor: isDark
+              ? appColors.neutral.gray[700]
+              : appColors.neutral.gray[200],
+          },
+          tabBarLabelStyle: {
+            fontSize: 16,
+            fontWeight: '600',
+            textTransform: 'none',
+          },
+          tabBarActiveTintColor: isDark
+            ? appColors.primary.light
+            : appColors.primary.dark,
+          tabBarInactiveTintColor: isDark
+            ? appColors.neutral.gray[400]
+            : appColors.neutral.gray[600],
+          tabBarIndicatorStyle: {
+            backgroundColor: isDark
               ? appColors.primary.light
               : appColors.primary.dark,
-            tabBarInactiveTintColor: isDark
-              ? appColors.neutral.gray[400]
-              : appColors.neutral.gray[600],
-            tabBarIndicatorStyle: {
-              backgroundColor: isDark
-                ? appColors.primary.light
-                : appColors.primary.dark,
-              height: 3,
-            },
-            swipeEnabled: true,
-          }}>
-          <Tab.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{
-              tabBarLabel: t('authentication.signIn'),
-            }}
-          />
+            height: 3,
+          },
+          swipeEnabled: true,
+        }}>
+        <Tab.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            tabBarLabel: t('authentication.signIn'),
+          }}
+        />
 
-          <Tab.Screen
-            name="Register"
-            component={RegisterScreen}
-            options={{
-              tabBarLabel: t('authentication.signUp'),
-            }}
-          />
-        </Tab.Navigator>
-      </View>
+        <Tab.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{
+            tabBarLabel: t('authentication.signUp'),
+          }}
+        />
+      </Tab.Navigator>
     </SafeAreaView>
   );
 };
